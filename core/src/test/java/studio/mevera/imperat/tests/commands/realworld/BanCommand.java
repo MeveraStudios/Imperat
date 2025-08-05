c93bb915315782241bb136a2339e48bb5d7a6ee0
Mqzn <mezoahmed2507@gmail.com>
2025-08-05 16:11:24 +0300

    Fixed test errors

--- core/src/test/java/studio/mevera/imperat/tests/commands/realworld/BanCommand.java
+++ core/src/test/java/studio/mevera/imperat/tests/commands/realworld/BanCommand.java
@@ -21,7 +21,7 @@ public final class BanCommand {
             @Named("target") String player,
             @Switch({"silent", "s"}) boolean silent,
             @Switch("ip") boolean ip,
-            @Named("duration") @Default("permanent") @Nullable Duration duration,
+            @Named("duration") @Default("permanent") @Nullable String duration,
             @Named("reason") @Default("Breaking server laws") @Optional @Greedy String reason
     ) {
         //TODO actual ban logic
