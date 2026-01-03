9ddd978315ef35488d1788f9d16f52d53aaa3c3f
iiAhmedYT <61851106+iiAhmedYT@users.noreply.github.com>
2026-01-03 19:10:18 +0200

    fix: tab completion overriding other commands (thanks 5170 for not reporting this!)

--- bukkit/src/main/java/studio/mevera/imperat/AsyncTabListener.java
+++ bukkit/src/main/java/studio/mevera/imperat/AsyncTabListener.java
@@ -5,22 +5,27 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 public class AsyncTabListener implements Listener {
-    
+
     private final BukkitImperat imperat;
-    
+
     public AsyncTabListener(BukkitImperat imperat) {
         this.imperat = imperat;
     }
 
     @EventHandler
     public void asyncTabComplete(AsyncTabCompleteEvent event) {
-        BukkitSource src = imperat.wrapSender(event.getSender());
         String commandLine = event.getBuffer();
-        if(commandLine.startsWith("/")) {
+        if (commandLine.startsWith("/")) {
             commandLine = commandLine.substring(1);
         }
-        event.setCompletions(
-                imperat.autoComplete(src, commandLine).join()
-        );
+
+        BukkitSource src = imperat.wrapSender(event.getSender());
+        var automplete = imperat.autoComplete(src, commandLine).join();
+        if (automplete.isEmpty()) {
+            return;
+        }
+
+        event.setCompletions(automplete);
     }
+
 }
