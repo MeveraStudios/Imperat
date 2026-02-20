9fe32a62f1859037652b0599a08d45cf6bdc6ec5
Mqzn <mezoahmed2507@gmail.com>
2026-02-20 04:34:15 +0200

    Rename Priority annotation to ParseOrder and update references in SourceOrderHelper for consistency

--- core/src/main/java/studio/mevera/imperat/annotations/ParseOrder.java
+++ core/src/main/java/studio/mevera/imperat/annotations/ParseOrder.java
@@ -13,7 +13,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Documented
-public @interface Priority {
+public @interface ParseOrder {
 
     /**
      * The priority value.
