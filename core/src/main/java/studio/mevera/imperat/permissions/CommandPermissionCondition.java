851b3030aaa035bae1fd0f5b160d975eb9fc9845
Mqzn <mezoahmed2507@gmail.com>
2026-03-11 05:47:53 +0200

    fixed access issue

--- core/src/main/java/studio/mevera/imperat/permissions/CommandPermissionCondition.java
+++ core/src/main/java/studio/mevera/imperat/permissions/CommandPermissionCondition.java
@@ -8,7 +8,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-class CommandPermissionCondition {
+public class CommandPermissionCondition {
 
     private final static String AND_SYMBOL = "&";
     private final static String OR_SYMBOL = "|";
